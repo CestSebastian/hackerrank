e10d55c31a6409a5afa1c4eacda474ec28fd5fb3
Sebastian Rus <sebastian.s.rus+gituser@gmail.com>
2013-08-23 14:17:50 +0300

    final

--- sorting/sorting_prob3.js
+++ sorting/sorting_prob3.js
@@ -1,16 +1,62 @@
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 
-function countShifts (a) {
-    var shifts = 0;
+var swaps = 0;
+
+function merge(v, i, mid, j) {
+    var x = i,
+        y = mid + 1,
+        aux = [];
     
-    for (var i = 1; i < a.length; i++) {
-        for (var j = 0; j < i; j++) {
-            if (a[i] < a[j]) shifts++;
+    while (x <= mid && y <= j) {
+        if (v[x] <= v[y]) {
+            aux.push(v[x]);
+            x++;
+        } else {
+            aux.push(v[y]);
+            y++;
+            swaps+= mid - x + 1;
         }
     }
     
-    return shifts;
+    if (x <= mid) {
+        for (x; x <= mid; x++) {
+            aux.push(v[x]);
+        }
+    }
+    
+    if (y <= j) {
+        for (y; y <= j; y++) {
+            aux.push(v[y]);
+        }
+    }
+    
+    for (var k = i; k <= j; k++) {
+        v[k] = aux[k - i];
+    }
+}
+
+function merge_sort(v, i, j) {
+    if (i < j) {
+        var mid = Math.floor((i + j) / 2);
+        merge_sort(v, i, mid);
+        merge_sort(v, mid + 1, j);
+        merge(v, i, mid, j);
+    }
+}
+
+function countShifts (a) {
+    swaps = 0;
+    
+    merge_sort(a, 0, a.length - 1);
+    
+//     for (var i = 1; i < a.length; i++) {
+//         for (var j = 0; j < i; j++) {
+//             if (a[i] < a[j]) shifts++;
+//         }
+//     }
+    
+    return swaps;
 }
 
 var input = '';
@@ -27,13 +73,14 @@ process.stdin.on("end", function () {
     
     for (var i=1;i<=count;i++) {
         lengths.push(Number(inArray[(i-1) * 2 + 1]));
+        
+        if (!inArray[(i - 1) * 2 + 2]) {
+            process.stdout.write(i + '\n');
+        }
         arrays.push(inArray[(i - 1) * 2 + 2].split(' ').map(Number));
     }
-    console.time('AllTime');
+    
     for (var i=0; i<arrays.length; i++) {
-        console.time('time_' + i);
         process.stdout.write(countShifts(arrays[i]) + '\n');
-        console.timeEnd('time_' + i);
     }
-    console.timeEnd('AllTime');
 });
